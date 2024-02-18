a00d8f96e709a04c5a9b89ca971c8c15dea2ed77
Advaiyt Sane <advaiyt.sane@gmail.com>
2024-02-18 01:07:14 +0000

    Connected camera and temperature

--- TemperatureAndCamera.py
+++ TemperatureAndCamera.py
@@ -24,22 +24,32 @@ riskAss = RiskAssessment()
 try:
     while True:
         # print("attempted reading at :", time.time())
-
         result = myDHT.read()
         if result.is_valid():
             t = result.temperature
             h = result.humidity
+            isValdTemp = True
             print("Temperature is " , t, h," at ", time.time())
             lcd_1602.write(0,0, "Temp is {0}".format(t))
             
-        lcd_1602.write(0,1, "potential danger")
         frame =  piCam.capture_array()
         riskAss.update(t, frame)
-        face_locations = riskAss.get_face()
-        if riskAss.is_face():
-            print("baby detected and temperature above 80 degrees! Please check your car. ")
+        isFaceDetected = riskAss.is_face()    
+        if isFaceDetected:
+            # Overlay facedetection on image
+            face_locations = riskAss.get_face()
+            # print("baby detected and temperature above 80 degrees! Please check your car. ")
             for x, y, w, h  in face_locations:
                 cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+        if riskAss.temp_warning():
+            lcd_1602.write(0,1, "Warn! High Temp!")
+            if isFaceDetected:
+                lcd_1602.write(0,1, "Danger! Baby in High Temp!")
+        else:
+            if isFaceDetected:
+                lcd_1602.write(0,1, "Warn! Baby onboard!")
+            else:
+                lcd_1602.write(0,1, "  Safe!       ")
         cv2.imshow("piCam", frame)
 
         if cv2.waitKey(1) == ord('q'):
